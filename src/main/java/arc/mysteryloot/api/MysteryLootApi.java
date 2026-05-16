b8c94122c1fe20b6a81ba9f379111376ea4c4d8c
elementarc <arctale.void@gmail.com>
2026-05-17 00:53:29 +0200

    hotfixes

--- src/main/java/arc/mysteryloot/api/MysteryLootApi.java
+++ src/main/java/arc/mysteryloot/api/MysteryLootApi.java
@@ -11,7 +11,6 @@ import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 
 import arc.mysteryloot.MysteryLootPlugin;
 import arc.mysteryloot.classes.MysteryLootTable;
-import arc.mysteryloot.classes.MysteryLootTableItem;
 
 /**
  * Public API for the Arc Mystery Loot plugin.
@@ -29,52 +28,10 @@ public class MysteryLootApi {
     return MysteryLootPlugin.INSTANCE.Manager.GetAllLootTables();
   }
 
-  public static boolean CreateMysteryLootTable(String tableId) {
-    return MysteryLootPlugin.INSTANCE.Manager.CreateMysteryLootTable(tableId);
-  }
-
-  public static boolean DeleteMysteryLootTable(String tableId) {
-    return MysteryLootPlugin.INSTANCE.Manager.DeleteMysteryLootTable(tableId);
-  }
-
-  public static boolean UpdateMysteryLootTable(String tableId, MysteryLootTable newTable) {
-    return MysteryLootPlugin.INSTANCE.Manager.UpdateMysteryLootTable(tableId, newTable);
-  }
-
-  public static boolean RenameMysteryLootTable(String oldId, String newId, MysteryLootTable table) {
-    return MysteryLootPlugin.INSTANCE.Manager.RenameMysteryLootTable(oldId, newId, table);
-  }
-
-  @Nullable
-  public static MysteryLootTableItem GetLootTableItem(String tableId, int itemIndex) {
-    return MysteryLootPlugin.INSTANCE.Manager.GetLootTableItem(tableId, itemIndex);
-  }
-
-  public static boolean CreateLootTableItem(String tableId, MysteryLootTableItem item) {
-    return MysteryLootPlugin.INSTANCE.Manager.CreateLootTableItem(tableId, item);
-  }
-
-  public static boolean UpdateLootTableItem(String tableId, int itemIndex, MysteryLootTableItem updatedItem) {
-    return MysteryLootPlugin.INSTANCE.Manager.UpdateLootTableItem(tableId, itemIndex, updatedItem);
-  }
-
-  public static boolean DeleteLootTableItem(String tableId, int itemIndex) {
-    return MysteryLootPlugin.INSTANCE.Manager.DeleteLootTableItem(tableId, itemIndex);
-  }
-
-  /**
-   * Performs a weighted random roll on the given loot table.
-   * Returns the rolled item, or null if the table doesn't exist or is empty.
-   */
-  @Nullable
-  public static MysteryLootTableItem RollLootTable(String tableId) {
-    return MysteryLootPlugin.INSTANCE.Manager.RollLootTable(tableId);
-  }
-
   /**
    * Opens the player-facing Mystery Loot table page for the given table.
    * Call this from your own plugin when a player interacts with a loot source.
-   */
+  */
   public static void OpenMysteryLootTablePage(
     @Nonnull Ref<EntityStore> ref,
     @Nonnull Store<EntityStore> store,
